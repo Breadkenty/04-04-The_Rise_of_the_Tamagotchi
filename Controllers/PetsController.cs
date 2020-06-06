b64143cbb61d9c010d5aa57b45edaa8015307022
Kento Kawakami <kawakamikento@gmail.com>
2020-06-06 00:06:48 -0400

    added Delete

--- Controllers/PetsController.cs
+++ Controllers/PetsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using _04_04_The_Rise_of_the_Tamagotchi.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace _04_04_The_Rise_of_the_Tamagotchi.Controllers
 {
@@ -54,5 +55,45 @@ namespace _04_04_The_Rise_of_the_Tamagotchi.Controllers
 
             return CreatedAtAction(null, null, petToCreate);
         }
+
+        [HttpPut("{id}")]
+        public ActionResult<Pet> Update(int id, Pet petUpdate)
+        {
+            var selectedPet = _context.Pets.FirstOrDefault(pet => pet.Id == id);
+
+            if (selectedPet == null)
+            {
+                return NotFound();
+            }
+
+            selectedPet.Name = petUpdate.Name;
+            selectedPet.Birthday = petUpdate.Birthday;
+            selectedPet.HungerLevel = petUpdate.HungerLevel;
+            selectedPet.HappinessLevel = petUpdate.HappinessLevel;
+            selectedPet.LastInteracted = petUpdate.LastInteracted;
+            selectedPet.IsDead = petUpdate.IsDead;
+
+            _context.Entry(selectedPet).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(petUpdate);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Pet> Delete(int id)
+        {
+            var selectedPet = _context.Pets.FirstOrDefault(pet => pet.Id == id);
+
+            if (selectedPet == null)
+            {
+                return NotFound();
+            }
+
+            _context.Pets.Remove(selectedPet);
+            _context.SaveChanges();
+
+            return Ok(selectedPet);
+        }
+
     }
 }
